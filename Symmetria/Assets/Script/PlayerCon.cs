ac4e60de52cd1c8935c51e66feb442f82998aa8f
ShunyaYokoe <nkcgta.shunya.yokoe@gmail.com>
2021-10-21 00:04:37 +0900

    playerScript修正

--- Symmetria/Assets/Script/PlayerCon.cs
+++ Symmetria/Assets/Script/PlayerCon.cs
@@ -6,46 +6,22 @@ public class PlayerCon : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     Animator animator;
-    float walkForce;
-    float maxWalkSpeed;
 
     /// <summary>
-    /// プレイヤーの向き
+    /// 家具の配列
     /// </summary>
-    int key = 0;
+    GameObject[] furniture;
 
+    bool parents_set;
 
-    // Start is called before the first frame update
     void Start()
     {
         rigid2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
-    }
-
-    /// <summary>
-    /// 移動
-    /// </summary>
-    void PlayerMove()
-    {
-        //待機
-        if(key==0)
-        {
-
-        }
-
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            key = 1;
-        }
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            key = -1;
-        }
     }
 }
