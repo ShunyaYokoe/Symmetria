738c169d2ba97b46089457a327c4e681bac1a1e9
ShunyaYokoe <nkcgta.shunya.yokoe@gmail.com>
2021-10-28 01:34:13 +0900

    Script修正

--- Symmetria/Assets/Script/PlayerCon.cs
+++ Symmetria/Assets/Script/PlayerCon.cs
@@ -6,27 +6,32 @@ public class PlayerCon : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     Animator animator;
+    float walkforce = 20.0f;
+    float maxwalkspeed = 1.5f;
 
     /// <summary>
     /// 家具の配列
     /// </summary>
-    GameObject[] furniture;
-
+    GameObject[] furnitures;
     bool parents_set;
-
-    bool direction;
+    bool player_dire;
 
     int key = 0;
 
     void Start()
     {
+        furnitures = GameObject.FindGameObjectsWithTag("furniture");
+
         rigid2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        parents_set = false;
+        player_dire = false;
     }
 
     void Update()
     {
-        
+
     }
 
     void Player_move()
@@ -34,13 +39,13 @@ public class PlayerCon : MonoBehaviour
         if(Input.GetKey(KeyCode.RightArrow))
         {
             key = 1;
-            direction = true;
+            player_dire = true;
         }
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             key = -1;
-            direction = false;
+            player_dire = false;
         }
     }
 }
